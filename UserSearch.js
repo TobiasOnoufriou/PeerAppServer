4fc4a76fd9b0dcb1d23d930b1eb5723ef2faceb1
TobiasOnoufriou <spacehomer@live.co.uk>
2020-03-31 01:50:35 +0100

    Added so it can't search

--- UserSearch.js
+++ UserSearch.js
@@ -53,8 +53,9 @@ var UserSearch = /** @class */ (function () {
                     case 0:
                         user = this.name;
                         console.log(user);
+                        this._id = this._id.substring(1, this._id.length - 1);
                         rgx = new RegExp("^" + user);
-                        return [4 /*yield*/, this.db.collection("UserData").find({ username: rgx }).toArray()
+                        return [4 /*yield*/, this.db.collection("UserData").find({ $and: [{ username: rgx }, { _id: new mongodb_1.ObjectId(this._id) }] }).toArray()
                                 .then(function (doc) {
                                 return doc;
                             })];
--- UserSearch.ts
+++ UserSearch.ts
@@ -23,9 +23,10 @@ export class UserSearch{
         let user:string = this.name;
         let users;
         console.log(user);
+        this._id = this._id.substring(1, this._id.length -1);
         var rgx = new RegExp("^" + user);
         //Problem here with the searching of users.
-       users = await this.db.collection("UserData").find({username: rgx}).toArray()
+       users = await this.db.collection("UserData").find({$and:[{username: rgx},{_id:new ObjectId(this._id)}]}).toArray()
         .then(doc =>{
             return doc;
         });
